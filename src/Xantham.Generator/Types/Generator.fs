0b6c807d4623672af4654d72c639905608c46a89
shayanhabibi <shayan.habibi01@gmail.com>
2026-04-11 13:49:07 +0800

    feat: machinery for hooks

--- src/Xantham.Generator/Types/Generator.fs
+++ src/Xantham.Generator/Types/Generator.fs
@@ -3,6 +3,7 @@
 open System.Collections.Generic
 open System.ComponentModel
 open Xantham.Decoder.ArenaInterner
+open Xantham.Generator.NamePath
 open Xantham.Generator.TypeRefRender
 open Xantham.Generator
 
@@ -15,18 +16,31 @@ type DictionaryImpl<'Key, 'Value> =
     Dictionary<'Key, 'Value>
     #endif
 
-type GeneratorContext =
+type GeneratorHooks = {
+    /// if you want default handling for the resolved type, you can return None
+    TypeRefPrerender: (GeneratorContext -> ResolvedType -> TypeRefRender option) list
+    ExportRefPrerender: (GeneratorContext -> ResolvedExport -> TypeRefRender option) list
+    /// if you want default handling for the typeRefRender, you can return the original
+    TypeRefPostrender: (GeneratorContext -> TypeRefRender -> TypeRefRender) list
+    PathModifier: ( GeneratorContext -> AnchorPath -> AnchorPath ) list
+    /// If yyou want to keep the default render, just return the argument
+    TypeRenderModifier: ( GeneratorContext -> TypeRender -> TypeRender ) list
+}
+
+and GeneratorContext =
     {
         TypeRefRenders: DictionaryImpl<ResolvedType, TypeRefRender>
         ExportRefRenders: DictionaryImpl<ResolvedExport, TypeRefRender>
         TypeRenders: DictionaryImpl<ResolvedType, Render>
         ExportRenders: DictionaryImpl<ResolvedExport, Render>
+        Hooks: GeneratorHooks option
     }
     static member Empty = {
         TypeRefRenders = DictionaryImpl<ResolvedType, TypeRefRender>()
         ExportRefRenders = DictionaryImpl<ResolvedExport, TypeRefRender>()
         TypeRenders = DictionaryImpl<ResolvedType, Render>()
         ExportRenders = DictionaryImpl<ResolvedExport, Render>()
+        Hooks = None
     }
     override this.ToString() = $"GeneratorContext(%d{this.TypeRefRenders.Count})"
 
@@ -58,10 +72,103 @@ module GeneratorContext =
             Dictionary.addOrReplace key value dict
             #endif
 
+    let private makeTypeRefRenderHooks (context: GeneratorContext) (fn: ResolvedType -> TypeRefRender) =
+        let postFunc =
+            match context.Hooks with
+            | None | Some { TypeRefPostrender = [] } -> id
+            | Some { TypeRefPostrender = hooks } ->
+                fun typeRefRender ->
+                    hooks
+                    |> List.fold (fun typeRefRender fn -> fn context typeRefRender) typeRefRender
+        match context.Hooks with
+        | None | Some { TypeRefPrerender = [] } -> fn
+        | Some { TypeRefPrerender = hooks } ->
+            fun resolvedType ->
+                hooks
+                |> List.tryPick (fun hook -> hook context resolvedType)
+                |> Option.defaultValue (fn resolvedType)
+        >> postFunc
+    let private makeExportRefRenderHooks (context: GeneratorContext) (fn: ResolvedExport -> TypeRefRender) =
+        let postFunc =
+            match context.Hooks with
+            | None | Some { TypeRefPostrender = [] } -> id
+            | Some { TypeRefPostrender = hooks } ->
+                fun typeRefRender ->
+                    hooks
+                    |> List.fold (fun typeRefRender fn -> fn context typeRefRender) typeRefRender
+        match context.Hooks with
+        | None | Some { ExportRefPrerender = [] } -> fn
+        | Some { ExportRefPrerender = hooks } ->
+            fun resolvedExport ->
+                hooks
+                |> List.tryPick (fun hook -> hook context resolvedExport)
+                |> Option.defaultValue (fn resolvedExport)
+        >> postFunc
+    module Hooks =
+        let withHooks (hooks: GeneratorHooks) (context: GeneratorContext) =
+            { context with Hooks = Some hooks }
+        let empty = {
+            GeneratorHooks.ExportRefPrerender = []
+            TypeRefPrerender = []
+            TypeRefPostrender = []
+            PathModifier = []
+            TypeRenderModifier = []
+        }
+        let forHooks (hookFn: GeneratorHooks -> GeneratorHooks) (context: GeneratorContext) =
+            { context with Hooks = context.Hooks |> Option.defaultValue empty |> hookFn |> Some }
+        module Path =
+            /// <summary>
+            /// Allows you to modify paths that are generated. The hooks are executed
+            /// in order. Each subsequent hook will act on the output of the previous one.
+            /// </summary>
+            /// <param name="handler"></param>
+            /// <param name="context"></param>
+            let withHandler (handler: GeneratorContext -> AnchorPath -> AnchorPath) (context: GeneratorContext) =
+                forHooks (fun hooks -> {
+                    hooks with PathModifier = hooks.PathModifier @ [ handler ]
+                }) context
+        module TypeRef =
+            /// <summary>
+            /// Allows you to modify the typeRefRender that is generated for a ResolvedType.
+            /// The hooks are executed in order. You can choose to allow the default behaviour by
+            /// returning None. Hooks are executed until one of them returns <c>Some</c> value.
+            /// </summary>
+            /// <param name="handler"></param>
+            /// <param name="context"></param>
+            let forResolvedType (handler: GeneratorContext -> ResolvedType -> TypeRefRender option) (context: GeneratorContext) =
+                forHooks (fun hooks -> {
+                    hooks with TypeRefPrerender = hooks.TypeRefPrerender @ [ handler ]
+                }) context
+               
+            /// <summary>
+            /// Allows you to modify the typeRefRender that is generated for a ResolvedExport.
+            /// The hooks are executed in order. You can choose to allow the default behaviour by
+            /// returning None. Hooks are executed until one of them returns <c>Some</c> value.
+            /// </summary>
+            /// <param name="handler"></param>
+            /// <param name="context"></param>
+            let forExport (handler: GeneratorContext -> ResolvedExport -> TypeRefRender option) (context: GeneratorContext) =
+                forHooks (fun hooks -> {
+                    hooks with ExportRefPrerender = hooks.ExportRefPrerender @ [ handler ]
+                }) context
+            /// <summary>
+            /// Allows you to modify type ref renders after they have been generated; agnostic of whether
+            /// they were generated by a ResolvedType or ResolvedExport. The hooks are executed in order.
+            /// Each subsequent hook will act on the output of the previous one.
+            /// </summary>
+            /// <param name="handler"></param>
+            /// <param name="context"></param>
+            let withPosthandler (handler: GeneratorContext -> TypeRefRender -> TypeRefRender) (context: GeneratorContext) =
+                forHooks (fun hooks -> {
+                    hooks with TypeRefPostrender = hooks.TypeRefPostrender @ [ handler ]
+                }) context
+                
     let getTypeRefWith (context: GeneratorContext) (resolvedType: ResolvedType) (f: ResolvedType -> TypeRefRender) =
-        Operation.getOrAdd f resolvedType context.TypeRefRenders
+        let pipeline = makeTypeRefRenderHooks context f
+        Operation.getOrAdd pipeline resolvedType context.TypeRefRenders
     let getExportRefWith (context: GeneratorContext) (resolvedExport: ResolvedExport) (f: ResolvedExport -> TypeRefRender) =
-        Operation.getOrAdd f resolvedExport context.ExportRefRenders
+        let pipeline = makeExportRefRenderHooks context f
+        Operation.getOrAdd pipeline resolvedExport context.ExportRefRenders
     let getTypeRef (context: GeneratorContext) (resolvedType: ResolvedType) =
         Operation.tryGet resolvedType context.TypeRefRenders
     let getExportRef (context: GeneratorContext) (resolvedExport: ResolvedExport) =
