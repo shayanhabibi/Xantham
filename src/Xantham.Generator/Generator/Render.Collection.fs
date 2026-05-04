32afa0cd93826efe8d47f94efa3872ad2aa5a5d8
houstonhaynes <8174976+houstonhaynes@users.noreply.github.com>
2026-05-04 16:52:57 -0400

    add guard to renderRoot; update tests

--- src/Xantham.Generator/Generator/Render.Collection.fs
+++ src/Xantham.Generator/Generator/Render.Collection.fs
@@ -386,6 +386,7 @@ let renderRoot (ctx: GeneratorContext) (root: RootModule) =
                 LiteralUnionRender.renderEnum ctx literalUnionRender
             | _ -> ()
         for module' in root.Modules.Values do
-            renderModuleInterface ctx module'
+            if module'.Members.Count > 0 then
+                renderModuleInterface ctx module'
         yield! nextModules
     }
