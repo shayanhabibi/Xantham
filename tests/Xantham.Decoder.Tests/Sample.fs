d9e6b4d8fa86c14acc824a107ad86a79d62e2ad2
shayanhabibi <shayan.habibi01@gmail.com>
2026-05-05 00:35:27 +0800

    chore: ci tests now test major packages for regression in the capacity of the encoder or decoder to parse/decode

---
+++ tests/Xantham.Decoder.Tests/Sample.fs
@@ -0,0 +1,145 @@
+module Fixture
+
+open EasyBuild.FileSystemProvider
+open Expecto
+open Fake.JavaScript
+open Xantham.Decoder
+
+[<Literal>]
+let repoRoot = __SOURCE_DIRECTORY__ + "/../.."
+
+type Root = AbsoluteFileSystem<__SOURCE_DIRECTORY__>
+type RepoRoot = AbsoluteFileSystem<repoRoot>
+
+module Npm =
+    let setDir dir  = fun param -> { param with Npm.NpmParams.WorkingDirectory = dir }
+    
+type Fixture = {
+    Name: string
+    TypeDefinitionFile: string
+    Output: string
+    Root: string
+} with override this.ToString() = this.Name
+
+module Virtual =
+    [<Literal>]
+    let agentsFixtureLayout = "
+            node_modules/
+                agents/
+                    dist/
+                        index.d.ts
+            output.json"
+    [<Literal>]
+    let solidJsFixtureLayout = "
+            node_modules/
+                solid-js/
+                    types/
+                        index.d.ts
+            output.json"
+    [<Literal>]
+    let threeFixtureLayout = "
+            node_modules/
+                @types/
+                    three/
+                        index.d.ts
+            output.json"
+    [<Literal>]
+    let cloudFlareFixtureLayout = "
+            node_modules/
+                @cloudflare/
+                    dynamic-workflows/
+                        dist/
+                            index.d.ts
+                    workers-types/
+                        dist/
+                            index.d.ts
+            output.json"
+    
+    type Agents =
+        VirtualFileSystem<Root.fixtures.agents.``.``, agentsFixtureLayout>
+    type SolidJs =
+        VirtualFileSystem<Root.fixtures.``solid-js``.``.``, solidJsFixtureLayout>
+    type Three =
+        VirtualFileSystem<Root.fixtures.three.``.``, threeFixtureLayout>
+    type DynamicWorkflows =
+        VirtualFileSystem<Root.fixtures.``dynamic-workflows``.``.``, cloudFlareFixtureLayout>
+    type WorkersTypes =
+        VirtualFileSystem<Root.fixtures.``workers-types``.``.``, cloudFlareFixtureLayout>
+        
+
+module Fixtures =
+    Npm.setDir RepoRoot.``.``
+    |> Npm.run "build"
+
+    let setup fixture =
+        if System.IO.Path.Exists(fixture.TypeDefinitionFile) then fixture else
+        Npm.setDir fixture.Root
+        |> Npm.install
+        fixture
+    
+    let encode fixture =
+        try
+        Npm.setDir RepoRoot.``.``
+        |> Npm.exec $"exec . -- {fixture.TypeDefinitionFile} -o {fixture.Output}"
+        Ok fixture
+        with e -> Error(e)
+    let decode fixture =
+        match fixture with
+        | Ok fixture ->
+            try
+            Runtime.create fixture.Output
+            |> Ok
+            with e -> Error e
+        | Error e -> Error e
+    
+    let agents = {
+        Name = "Agents"
+        TypeDefinitionFile = Virtual.Agents.node_modules.agents.dist.``index.d.ts``
+        Output = Virtual.Agents.``output.json``
+        Root = Root.fixtures.agents.``.``
+    }
+    let solidjs = {
+        Name = "SolidJs"
+        TypeDefinitionFile = Virtual.SolidJs.node_modules.``solid-js``.types.``index.d.ts``
+        Output = Virtual.SolidJs.``output.json``
+        Root = Root.fixtures.``solid-js``.``.``
+    }
+    let three = {
+        Name = "Three"
+        TypeDefinitionFile = Virtual.Three.node_modules.``@types``.three.``index.d.ts``
+        Output = Virtual.Three.``output.json``
+        Root = Root.fixtures.three.``.``
+    }
+    let dynamicWorkflows = {
+        Name = "Dynamic Workflows"
+        TypeDefinitionFile = Virtual.DynamicWorkflows.node_modules.``@cloudflare``.``dynamic-workflows``.dist.``index.d.ts``
+        Output = Virtual.DynamicWorkflows.``output.json``
+        Root = Root.fixtures.``dynamic-workflows``.``.``
+    }
+    let workersTypes = {
+        Name = "Workers Types"
+        TypeDefinitionFile = Virtual.WorkersTypes.node_modules.``@cloudflare``.``workers-types``.dist.``index.d.ts``
+        Output = Virtual.WorkersTypes.``output.json``
+        Root = Root.fixtures.``workers-types``.``.``
+    }
+
+let inline funApply f fn = fn f
+
+[<Tests>]
+let tests =
+    testTheory "Decoder" [
+        Fixtures.agents
+        Fixtures.solidjs
+        Fixtures.three
+        Fixtures.dynamicWorkflows
+        Fixtures.workersTypes
+    ] <| fun fixture ->
+        Fixtures.setup fixture
+        |> Fixtures.encode
+        |> Fixtures.decode
+        |> Expect.isOk
+        |> funApply ""
+    
+
+    
+
